a802c93f505345b0c948f1b06bc14da552e07d7e
luisfelipert <lfghias232@gmail.com>
2022-08-22 16:39:02 -0300

    Updating stuffs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React from 'react';
+import '../styles/global.css'
 
+import "./_document";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
+  
---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { CompletedChallengers } from "../components/CompletedChallengers";
+
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/pages/Home.module.css";
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallengers />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
